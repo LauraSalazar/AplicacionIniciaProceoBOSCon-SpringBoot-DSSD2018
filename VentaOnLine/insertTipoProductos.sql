66b07a461134bcc015605a69d2fed54ed5f92984
analaurasalazar <analaurasalazar@192.168.1.2>
2018-12-15 17:30:38 -0300

    ARchivos Del proyecto DSSD2018

---
+++ VentaOnLine/insertTipoProductos.sql
@@ -0,0 +1,10 @@
+INSERT INTO ventaonline.productType (id,description,initials) VALUES (3,'Productos ALIMENTICIOS','ALIMEN');
+INSERT INTO ventaonline.productType (id,description,initials) VALUES (4,'Productos IMPORTADOS','IMPOR');
+INSERT INTO ventaonline.productType (id,description,initials) VALUES (5,'Productos LIMPIEZA','LIMP');
+INSERT INTO ventaonline.productType (id,description,initials) VALUES (6,'Productos JUGUETES','JUGUE');
+INSERT INTO ventaonline.productType (id,description,initials) VALUES (7,'Productos GALLETITAS','GALL');
+INSERT INTO ventaonline.productType (id,description,initials) VALUES (8,'Productos GASEOSAS','GASE');
+INSERT INTO ventaonline.productType (id,description,initials) VALUES (9,'Productos CRISTALERIA','CRIST');
+INSERT INTO ventaonline.productType (id,description,initials) VALUES (10,'Productos VAJILLA','VAJ');
+INSERT INTO ventaonline.productType (id,description,initials) VALUES (11,'Productos GOLOSINAS','GOLOS');
+
